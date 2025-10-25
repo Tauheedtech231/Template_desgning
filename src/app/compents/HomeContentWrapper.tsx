274de6706054d0a956653cac59ca4096d4faa7eb
M.Tauheed <tauheeddeveloper13@gmail.com>
2025-10-25 14:12:06 +0500

    add

--- src/app/compents/HomeContentWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; // This makes the component a Client Component
-
-import React from "react";
-
-export default function HomeContentWrapper() {
-  // Never render Home on any page
-  return null;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./compents/Navbar";
-import Footer from "./compents/Footer";
-import HomeContentWrapper from "./compents/HomeContentWrapper"; // Client wrapper
+ // Client wrapper
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,13 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
 
-        {/* Client component handles conditional rendering */}
-        <HomeContentWrapper />
 
         {children}
-        <Footer />
+      
       </body>
     </html>
   );
